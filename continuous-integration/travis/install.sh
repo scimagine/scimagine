98cdbf8903da4cded9f0c86905be0b7742aea65b
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-05-06 12:56:55 -0400

    Fix miniconda3 path

--- continuous-integration/travis/install.sh
+++ continuous-integration/travis/install.sh
@@ -3,9 +3,9 @@ MINICONDA_FILE="Miniconda3-latest-Linux-x86_64.sh"
 wget "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b
 
-export PATH=$HOME/miniconda/bin:$PATH
+export PATH=$HOME/miniconda3/bin:$PATH
 
-ls $HOME/miniconda/bin
+ls $HOME/miniconda3/bin
 
 conda update --yes conda
 conda install --yes pip conda-build jinja2 binstar
