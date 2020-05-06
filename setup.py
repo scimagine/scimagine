6dcee25642de411534a14bef42750dd67b3822e3
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2020-05-06 11:36:43 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+#####################################
+VERSION = "1.0"
+ISRELEASED = False
+__version__ = VERSION
+#####################################
+
+
+extension_module = Extension(
+    'scimagine.extension',
+     sources=['scimagine/extension.pyx']
+)
+
+
+if os.environ['CONDA_BUILD']:
+    with open('__conda_version__.txt', 'w') as f:
+        if ISRELEASED:
+            f.write(VERSION)
+        else:
+            f.write(VERSION + '.dev')
+
+setup(
+    name = 'scimagine',
+    version = VERSION,
+    description = 'This is a conda package that delivers a scimagine environment.',
+    ext_modules = cythonize([extension_module]),
+    zip_safe=False,
+    packages=['scimagine', 'scimagine.tests'],
+)
